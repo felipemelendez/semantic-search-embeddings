41b727cbba771c7ff570c6106bf5fbe423b557ea
felipe <felipe.melendez@pilot.com>
2025-05-05 06:14:17 -0700

    add env variables

--- supabase.js
+++ supabase.js
@@ -1,5 +1,10 @@
+import 'dotenv/config'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('https://qgycpbmliycrkgdqncot.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFneWNwYm1saXljcmtnZHFuY290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyODY5NDMsImV4cCI6MjA2MTg2Mjk0M30.rBP7LJju3HxYk1xq4N6rvQcR23YF7vrZIqUZWdoB-9k')
+const supabase = createClient(
+    process.env.SUPABASE_URL   || '',
+    process.env.SUPABASE_ANON_KEY || ''
+)
+
 
 export default supabase
